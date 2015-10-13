01f8c2c6434b993238d4bfa369468edb0ac0e0f6
BeiDream <331341164@qq.com>
2015-10-13 22:43:22 +0800

    文件调整

---
+++ src/BeiDream.Core/Dependency/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace BeiDream.Core.Dependency
+{
+    public class Bootstrapper
+    {
+        public IIocManager IocManager { get; private set; }
+        public Assembly Assembly { get; private set; }
+        public Bootstrapper(Assembly assembly)
+        {
+            IocManager = Dependency.IocManager.Instance;
+            Assembly = assembly;
+
+        }
+
+        public virtual void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly);
+        }
+    }
+}
