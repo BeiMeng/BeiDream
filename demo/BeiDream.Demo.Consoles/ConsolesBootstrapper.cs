3e6110fa0ef7e492104aa6184d69843b4dc466e3
BeiDream <331341164@qq.com>
2015-10-13 22:36:37 +0800

    添加失败文件调整

---
+++ demo/BeiDream.Demo.Consoles/ConsolesBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using BeiDream.Core.Dependency;
+
+namespace BeiDream.Demo.Consoles
+{
+    public class ConsolesBootstrapper : Bootstrapper
+    {
+        public ConsolesBootstrapper()
+            : base(Assembly.GetExecutingAssembly())
+        {
+        }
+
+        public override void Initialize()
+        {
+            //IocManager.AddConventionalRegistrar(new BasicConventionalRegistrar());
+            IocManager.Register<ITaskService, TaskService>(DependencyLifeStyle.Transient);
+            IocManager.Register<ITaskMange, TaskMange2>(DependencyLifeStyle.Transient);
+            base.Initialize();
+        }
+    }
+}
---
+++ demo/BeiDream.Demo.Service/ServiceBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using BeiDream.Core.Dependency;
+
+namespace BeiDream.Demo.Service
+{
+    public class ServiceBootstrapper:Bootstrapper
+    {
+
+        public ServiceBootstrapper() : base(Assembly.GetExecutingAssembly())
+        {   
+            
+        }
+    }
+}
