891caeee0df83d259ab03e0fbaafe9e0a92d4e30
BeiDream <331341164@qq.com>
2015-11-02 22:37:26 +0800

    用户角色设置功能修改

--- demo/BeiDream.Demo.Web/Areas/Systems/Controllers/UserController.cs
+++ demo/BeiDream.Demo.Web/Areas/Systems/Controllers/UserController.cs
@@ -136,5 +136,65 @@ namespace BeiDream.Demo.Web.Areas.Systems.Controllers
         {
             return PartialView("Parts/UserRoles", id);
         }
+        public ActionResult SetRoles(Guid userId,string ids)
+        {
+            _userService.SetRoles(userId, ToList<Guid>(ids));
+            return Json(new { Code = 1, Message = "保存成功！" });
+        }
+        #region 通用转换
+
+        /// <summary>
+        /// 转换为目标元素
+        /// </summary>
+        /// <typeparam name="T">目标类型</typeparam>
+        /// <param name="data">数据</param>
+        public static T To<T>(object data)
+        {
+            if (data == null)
+                return default(T);
+            if (data is string && string.IsNullOrWhiteSpace(data.ToString()))
+                return default(T);
+            Type type = GetType<T>();
+            try
+            {
+                if (type.Name.ToLower() == "guid")
+                    return (T)(object)new Guid(data.ToString());
+                if (data is IConvertible)
+                    return (T)Convert.ChangeType(data, type);
+                return (T)data;
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
+        /// <summary>
+        /// 转换为目标元素集合
+        /// </summary>
+        /// <typeparam name="T">目标元素类型</typeparam>
+        /// <param name="list">元素集合字符串，范例:83B0233C-A24F-49FD-8083-1337209EBC9A,EAB523C6-2FE7-47BE-89D5-C6D440C3033A</param>
+        public static List<T> ToList<T>(string list)
+        {
+            var result = new List<T>();
+            if (string.IsNullOrWhiteSpace(list))
+                return result;
+            var array = list.Split(',');
+            result.AddRange(from each in array where !string.IsNullOrWhiteSpace(each) select To<T>(each));
+            return result;
+        }
+        #region GetType(获取类型)
+
+        /// <summary>
+        /// 获取类型,对可空类型进行处理
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        public static Type GetType<T>()
+        {
+            return Nullable.GetUnderlyingType(typeof(T)) ?? typeof(T);
+        }
+
+        #endregion
+        #endregion
     }
 }
