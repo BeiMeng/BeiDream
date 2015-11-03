e64581f8f96a5004ca56b62ca9e2aed5e7caab91
BeiDream <331341164@qq.com>
2015-11-03 17:33:11 +0800

    SemanticUi测试项目添加

---
+++ web.Controls/BeiDream.Web.SemanticUi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BeiDream.Web.SemanticUi.Startup))]
+namespace BeiDream.Web.SemanticUi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
